cf0ceeae4c9d1d05cf3b07bb9502189f6a292f5b
Woody Lucas <wlucas0311@gmail.com>
2018-11-08 02:34:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
+<<<<<<< HEAD
   } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
     return `I love you, too.`;
+=======
+>>>>>>> 1be6ae34cdc66721aa9bad9dd0d0f8b5aa7c060e
   }
 }
